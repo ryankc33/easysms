d453d3c4f3a854c5e2bc302ead42464ba5975d72
ryan chew <ryan.layosh@gmail.com>
2013-12-05 16:19:56 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easysms::Application.config.secret_key_base = 'd73f5bb6f0c8b2207f82c97bf94f9152e282b433528050ebb4ab9f7215624253a1af2cad504aeea3e83361b59937afd3525501d3af9e1c66ee51d9f08001250d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Easysms::Application.config.session_store :cookie_store, key: '_easysms_session'
